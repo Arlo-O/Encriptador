6fa3f96d59d8301767ee0c078341bf666e40ede9
arlo-ocampo <arlonog.11@gmail.com>
2024-06-29 07:32:24 -0500

    Modificarción de la estructura del HTML para agregar el botón de copiar y su conexión con JavaScript, luego se realizaron cambios en el Styles para el diseño del boton, se desarrollo la lógica de encriptación y desencriptación del texto y se estableciaron condiciones de proceso y su proyección en la página, finalmente se desarrollo la funcionalidad del botón "copiar"

--- js/app.js
+++ js/app.js
@@ -7,15 +7,18 @@ La letra "u" es convertida para "ufat"
 */
 var textoEncriptar = '';
 var textoEncriptado = '';
+var textoDesencriptar = '';
+var textoDesencriptado = '';
 
 function encriptarTexto() {
-    textoEncriptar = document.getElementById('area_texto_original').value;
+    textoEncriptar = condicionesProceso();
     if (textoEncriptar == '') {
         return
     } else {
-        for (caracter in textoEncriptar){
+        for (let i = 0; i < textoEncriptar.length; i++){
+            caracter = textoEncriptar.charAt(i);
             switch (caracter) {
-                case 'a':
+                case "a":
                     textoEncriptado += 'ai';
                     break;
                 case 'e':
@@ -35,12 +38,57 @@ function encriptarTexto() {
                 break;
             }
         }
-        mostrarTextoEncriptado();
+        mostrarTextoEncriptado(textoEncriptado);
     }
 }
 
-function mostrarTextoEncriptado() {
-    document.getElementsByClassName('texto_historial_mensaje').value = textoEncriptado;
+function mostrarTextoEncriptado(texto) {
+    document.getElementById('anuncio__inicial').style.display = "none";
     document.getElementById('imagenCandado').style.display = "none";
-    console.log("encriptado");
+    document.getElementById('mensaje__procesado').textContent = texto;
+    document.getElementById('area_texto_original').value = '';
+    document.getElementById('copiarBTN').style.display = "flex";
+    document.getElementById('desencriptarBTN').removeAttribute('disabled')
+}
+
+function condicionesProceso() {
+    document.getElementById('mensaje__procesado').textContent = '';
+    textoEncriptado = '';
+    textoDesencriptado = '';
+    return document.getElementById('area_texto_original').value;
+}
+
+function desencriptarTexto() {
+    textoDesencriptar = condicionesProceso();
+    if (textoDesencriptar == '') {
+        return;
+    } else {
+        for (let i = 0; i < textoDesencriptar.length; i++){
+            caracter = textoDesencriptar.charAt(i);
+            textoDesencriptado += caracter;
+            switch (caracter) {
+                case "a":
+                    i += 1;
+                    break;
+                case 'e':
+                    i += 4;
+                    break;
+                case 'i':
+                    i +=3;
+                    break;
+                case 'o':
+                    i += 3;
+                    break;
+                case 'u':
+                    i += 3;
+                    break;
+            }
+        }
+        document.getElementById('mensaje__procesado').textContent = textoDesencriptado;
+        document.getElementById('area_texto_original').value = '';
+    }
+}
+
+function copiarTexto() {
+    navigator.clipboard.writeText(document.getElementById('mensaje__procesado').textContent);
 }
